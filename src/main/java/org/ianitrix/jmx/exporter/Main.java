d8a878a1ea93873824718e322132638e2d0dbb57
Guillaume Waignier <guillaume.waignier@gmail.com>
2019-04-15 17:26:17 +0200

    clean(code): clean code

--- src/main/java/org/ianitrix/jmx/exporter/Main.java
+++ src/main/java/org/ianitrix/jmx/exporter/Main.java
@@ -25,18 +25,14 @@ public class Main {
 		final ConsumerGroupOffsetExporter consumerGroupOffsetExporter = new ConsumerGroupOffsetExporter(
 				loadConfigurationFile(args));
 
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			consumerGroupOffsetExporter.stop();
-		}));
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> consumerGroupOffsetExporter.stop()));
 
 		consumerGroupOffsetExporter.start();
 	}
 
 	private static void checkArgument(String[] args) {
 		if (args.length != 1) {
-			log.error("Missing argument for configuration file");
-			System.out.println("Missing argument for configuration file");
-			System.out.println("Command line are : java - jar Main.jar config.properties");
+			log.error("Missing argument for configuration file\nCommand line are : java - jar Main.jar config.properties");
 			System.exit(1);
 		}
 	}
@@ -51,11 +47,9 @@ public class Main {
 			return properties;
 		} catch (final FileNotFoundException e) {
 			log.error("Missing configuration file {}", args[0], e);
-			System.out.println("Missing configuration file " + args[0]);
 			System.exit(1);
 		} catch (final IOException e) {
 			log.error("Error when loading configuration file", e);
-			System.out.println("Error when loading configuration file");
 			System.exit(1);
 		}
 		
