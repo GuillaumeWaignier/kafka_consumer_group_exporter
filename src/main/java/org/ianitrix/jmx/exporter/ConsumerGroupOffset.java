96ed233fb8859daacb7daeef7c63884280df3c0d
Guillaume Waignier <guillaume.waignier@gmail.com>
2019-04-17 22:12:09 +0200

    clean(code): remove useless code

--- src/main/java/org/ianitrix/jmx/exporter/ConsumerGroupOffset.java
+++ src/main/java/org/ianitrix/jmx/exporter/ConsumerGroupOffset.java
@@ -1,13 +1,15 @@
 package org.ianitrix.jmx.exporter;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Mbean used to expose consumer group offset
  * @author Guillaume Waignier
  *
  */
-@Data
+@Getter
+@Setter
 public class ConsumerGroupOffset implements ConsumerGroupOffsetMBean {
 	
 	private long value;
